081ec11e12815e1804dc2fe6c159d38bb91b0a12
Danila <74253232+Tanax-Xt@users.noreply.github.com>
2025-01-17 15:32:40 +0300

    тестим ci еще раз (не хочется менять докер)

--- solution/app.py
+++ solution/app.py
@@ -1,3 +1,6 @@
+import os
+
+import uvicorn
 from fastapi import FastAPI
 
 from api import api_router
@@ -5,3 +8,8 @@ from api import api_router
 app = FastAPI()
 
 app.include_router(api_router)
+
+if __name__ == "__main__":
+    server_address = os.getenv("SERVER_ADDRESS", "0.0.0.0:8080")
+    host, port = server_address.split(":")
+    uvicorn.run(app, host=host, port=int(port))
